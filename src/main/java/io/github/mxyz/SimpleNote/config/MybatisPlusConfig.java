d2b5f534e6ed2376adb9c1f961e0e6bd83a27557
mxyz1733 <3062317151@qq.com>
2025-10-18 19:57:42 +0800

    搭建基本Springboot+MySQL+MybatisPlus框架

---
+++ src/main/java/io/github/mxyz/SimpleNote/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package io.github.mxyz.SimpleNote.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("io.github.mxyz.SimpleNote.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //向Mybatis过滤器链中添加分页拦截器
+        interceptor.addInnerInterceptor(new
+                PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
---
+++ src/test/java/io/github/mxyz/SimpleNote/SimpleNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mxyz.SimpleNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
